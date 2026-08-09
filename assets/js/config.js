4a4e67389eecfa28cea17dbb4c6bf650d79474ad
parvezmosharafvu <308653838+parvezmosharafvu@users.noreply.github.com>
2026-08-09 06:37:54 +0600

    Change Supabase anon key in config.js
    Updated the Supabase anon key for configuration.

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,7 +4,7 @@ const SUPABASE_URL =
 'https://ohwzmxwsphsfzudmlins.supabase.co';
 
 const SUPABASE_ANON_KEY =
-'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9od3pteHdzcGhzZnp1ZG1saW5zIiwicm9sZSI6ImFub24iLCJ9.frTl7qnDx7SK2IBMQxFCkGe5u4XAQweRxPhQ-2r8rU';
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9od3pteHdzcGhzZnp1ZG1saW5zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYwMzE0MTksImV4cCI6MjEwMTYwNzQxOX0.frTl7qnDx7SK2IBMQxFCkKGe5u4XAQweRxPhQ-2r8rU';
 
 window.supabase =
 supabase.createClient(
